d4f54ac4bcc0dec069cbdc6617b9ee96ebf8ed77
github username <github email address>
2020-10-04 16:53:47 +0100

    commented out cors

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,7 +2,7 @@ class EmployeesController < ApplicationController
 
     def index 
     employee = Employee.all 
-    render json: employee
+    render json: employee, except:[:updated_at, :created_at]
     end
 
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,8 @@ class TasksController < ApplicationController
 
     def index 
     tasks = Task.all 
-    render json: tasks
+    render json: tasks, :except => [:updated_at]
     end
+
+
 end
