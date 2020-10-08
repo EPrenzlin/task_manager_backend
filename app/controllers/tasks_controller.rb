ef37b5b7b6da03669f2bcd93556445279876df54
github username <github email address>
2020-10-08 21:46:54 +0100

    task controller is able to delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,5 +5,9 @@ class TasksController < ApplicationController
     render json: tasks, :except => [:updated_at]
     end
 
+    def destroy 
+    task = Task.find_by(id: params[:id])
+    task.destroy
+    end
 
 end
