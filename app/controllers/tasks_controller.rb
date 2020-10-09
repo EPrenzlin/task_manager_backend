a1340154eb67e023a3649cd369880997e920f82f
github username <github email address>
2020-10-09 12:27:43 +0100

    able to make a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,14 @@ class TasksController < ApplicationController
     render json: tasks, :except => [:updated_at]
     end
 
+    def create 
+    task = Task.new(urgency:params[:urgency], description:params[:description], employee_id:params[:employee_id])
+    if task.valid? 
+        task.save 
+        render json: task 
+    end
+    end
+
     def destroy 
     task = Task.find_by(id: params[:id])
     task.destroy
